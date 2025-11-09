1b0eedf09ff8a5034347689843946f2afb713221
gustavooarantes <profissional.gustavoa@gmail.com>
2025-11-09 15:19:18 -0300

    [feat] Created the first project directory + its dockerfile, docker-compose.yaml, and application.properties properly configured (using environment variables)

---
+++ subscriptions-clean-arch/src/main/java/com/gustavooarantes/subscriptions_clean_arch/SubscriptionsCleanArchApplication.java
@@ -0,0 +1,13 @@
+package com.gustavooarantes.subscriptions_clean_arch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionsCleanArchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionsCleanArchApplication.class, args);
+	}
+
+}
---
+++ subscriptions-clean-arch/src/test/java/com/gustavooarantes/subscriptions_clean_arch/SubscriptionsCleanArchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavooarantes.subscriptions_clean_arch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionsCleanArchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
